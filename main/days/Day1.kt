74fa0b056844162b9ef01d1343bee5a5204af97b
Hannes Guedelhoefer <hannes@guedelhoefer.de>
2020-12-01 08:46:56 +0100

    :fire: Remove old solutions

---
+++ main/days/Day1.kt
@@ -0,0 +1,19 @@
+package me.reckter.aoc.days
+
+import me.reckter.aoc.Day
+import me.reckter.aoc.solution
+import me.reckter.aoc.solve
+
+class Day1 : Day {
+    override val day = 1
+
+    override fun solvePart1() {
+
+    }
+
+    override fun solvePart2() {
+
+    }
+}
+
+fun main(args: Array<String>) = solve<Day1>()
